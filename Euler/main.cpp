a51d9a3eb9845a927aec6955e47a1d952b94ad3c
Artur Mazurek <arturm@localhost.(none)>
2012-08-09 11:39:18 +0100

    Initial Commit

---
+++ Euler/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Euler
+//
+//  Created by Artur Mazurek on 09/08/2012.
+//  Copyright (c) 2012 Artur Mazurek. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
