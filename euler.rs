ed997fa549e7640a102c9d499da8e8f2f20bbdc3
Artur Mazurek <artur.s.mazurek@gmail.com>
2013-08-01 21:17:56 +0100

    Problem 2 in rust

--- euler.rs
+++ euler.rs
@@ -1,14 +1,33 @@
+use std::uint::range;
 
 fn problem1() {
 	let mut result = 0;
-	let mut i = 1;
-	loop {
+
+	for std::uint::range(0, 1000) |i| {
 		if i % 3 == 0 || i % 5 == 0 {
 			result += i;
 		}
+	}
 
-		i += 1;
-		if(i == 1000) {
+	println(fmt!("Result is %u", result));
+}
+
+fn problem2() {
+	let mut a = 1;
+	let mut b = 2;
+
+	let mut result = 0;
+
+	loop {
+		if b % 2 == 0 {
+			result += b;
+		}
+
+		let temp = b;
+		b = a + b;
+		a = temp;
+
+		if b > 4000000 {
 			break;
 		}
 	}
@@ -17,5 +36,5 @@ fn problem1() {
 }
 
 fn main() {
-	problem1();
+	problem2();
 }
