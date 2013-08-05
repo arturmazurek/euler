213e147d280e7d15e9d36f16ef108fbb72878a1f
Artur Mazurek <artur.s.mazurek@gmail.com>
2013-08-05 21:26:36 +0100

    More updates to big number class

---
+++ Euler/BigNumber.cpp
@@ -0,0 +1,172 @@
+//
+//  BigNumber.cpp
+//  Euler
+//
+//  Created by Artur Mazurek on 05.08.2013.
+//  Copyright (c) 2013 Artur Mazurek. All rights reserved.
+//
+
+#include "BigNumber.h"
+
+#include <ostream>
+
+BigNumber BigNumber::sMax = BigNumber{std::numeric_limits<unsigned long>::max()};
+
+BigNumber& BigNumber::concat(const BigNumber& other) {
+    mDigits.insert(mDigits.end(), other.mDigits.begin(), other.mDigits.end());
+    mCached.first = false;
+    
+    return *this;
+}
+
+BigNumber& BigNumber::operator+=(const BigNumber& other) {
+    // todo: do it in-place
+    mDigits = std::move(add(mDigits, other.mDigits));
+    mCached.first = false;
+    
+    return *this;
+}
+
+BigNumber& BigNumber::operator*=(const BigNumber& other) {
+    mDigits = std::move(multiply(mDigits, other.mDigits));
+    mCached.first = false;
+    
+    return *this;
+}
+
+BigNumber& BigNumber::operator/=(const BigNumber& other) {
+    assert(!"Unimplemented");
+    return *this;
+}
+
+size_t BigNumber::digits() const {
+    return mDigits.size();
+}
+
+unsigned long BigNumber::value() const {
+    if(!mCached.first) {
+        mCached.second = 0;
+        for(int i = 0; i < mDigits.size(); ++i) {
+            mCached.second += mDigits[i] * powul(10, i);
+        }
+        mCached.first = true;
+    }
+    
+    return mCached.second;
+}
+
+BigNumber::operator bool() const {
+    return value() != 0;
+}
+
+const std::vector<char> BigNumber::add(const std::vector<char>& a, const std::vector<char>& b) {
+    std::vector<char> result;
+    
+    char overflow = 0;
+    
+    auto end = std::max(a.size(), b.size());
+    for(int i = 0; i < end; ++i) {
+        overflow += i < a.size() ? a[i] : 0;
+        overflow += i < b.size() ? b[i] : 0;
+        
+        result.push_back(overflow % 10);
+        overflow /= 10;
+    }
+    
+    if(overflow) {
+        result.push_back(overflow);
+    }
+    
+    return result;
+}
+
+const std::vector<char> BigNumber::multiply(const std::vector<char>& a, const std::vector<char>& b) {
+    std::vector<char> result;
+    
+    for(int i = 0; i < a.size(); ++i) {
+        std::vector<char> temp(i, 0);
+        char overflow = 0;
+        
+        for(int j = 0; j < b.size(); ++j) {
+            overflow += a[i] * b[j];
+            temp.push_back(overflow % 10);
+            overflow /= 10;
+        }
+        
+        if(overflow) {
+            temp.push_back(overflow);
+        }
+        
+        result = add(result, temp);
+    }
+    
+    return result;
+}
+
+// Operators -----------------
+
+std::ostream& operator<<(std::ostream& s, const BigNumber& n) {
+    for(int i = (int)n.mDigits.size() -1; i >= 0; --i) {
+        s << (int)n.mDigits[i];
+    }
+    
+    return s;
+}
+
+bool operator<(const BigNumber& a, const BigNumber& b) {
+    if(a.digits() != b.digits()) {
+        return a.digits() < b.digits();
+    }
+    
+    for(int i = (int)a.digits()-1; i >= 0; --i) {
+        if(a.mDigits[i] != b.mDigits[i]) {
+            return a.mDigits[i] < b.mDigits[i];
+        }
+    }
+    
+    return false; //equal
+}
+
+bool operator>(const BigNumber& a, const BigNumber& b) {
+    return b < a;
+}
+
+bool operator<=(const BigNumber& a, const BigNumber& b) {
+    return !(a > b);
+}
+
+bool operator>=(const BigNumber& a, const BigNumber& b) {
+    return !(a < b);
+}
+
+bool operator==(const BigNumber& a, const BigNumber& b) {
+    if(a.digits() != b.digits()) {
+        return false;
+    }
+    
+    for(size_t i = 0; i < a.digits(); ++i) {
+        if(a.mDigits[i] != b.mDigits[i]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+bool operator!=(const BigNumber& a, const BigNumber& b) {
+    return !(a == b);
+}
+
+BigNumber operator+(const BigNumber& a, const BigNumber& b) {
+    BigNumber result{a};
+    result += b;
+    
+    return result;
+}
+
+BigNumber operator*(const BigNumber& a, const BigNumber& b) {
+    BigNumber result{a};
+    result *= b;
+    
+    return result;
+}
--- Euler/BigNumber.h
+++ Euler/BigNumber.h
@@ -10,7 +10,8 @@
 #define __Euler__BigNumber__
 
 #include <algorithm>
-#include <cassert>
+#include <cassert>  
+#include <limits>
 #include <vector>
 
 #include "util.h"
@@ -25,52 +26,17 @@ public:
         }
     }
     
-    BigNumber& concat(const BigNumber& other) {
-        mDigits.insert(mDigits.end(), other.mDigits.begin(), other.mDigits.end());
-        mCached.first = false;
-        
-        return *this;
-    }
+    BigNumber& concat(const BigNumber& other);
     
-    BigNumber& operator+=(const BigNumber& other) {
-        // todo: do it in-place
-        mDigits = std::move(add(mDigits, other.mDigits));
-        mCached.first = false;
+    BigNumber& operator+=(const BigNumber& other);
+    BigNumber& operator*=(const BigNumber& other);
+    BigNumber& operator/=(const BigNumber& other);
             
-        return *this;
-    }
+    size_t digits() const;
             
-    BigNumber& operator*=(const BigNumber& other) {
-        mDigits = std::move(multiply(mDigits, other.mDigits));
-        mCached.first = false;
-        
-        return *this;
-    }
-    
-    BigNumber& operator/=(const BigNumber& other) {
-        assert(!"Unimplemented");
-        return *this;
-    }
+    unsigned long value() const;
             
-    size_t digits() const {
-        return mDigits.size();
-    }
-            
-    unsigned long value() const {
-        if(!mCached.first) {
-            mCached.second = 0;
-            for(int i = 0; i < mDigits.size(); ++i) {
-                mCached.second += mDigits[i] * powul(10, i);
-            }
-            mCached.first = true;
-        }
-        
-        return mCached.second;
-    }
-            
-    operator bool() {
-        return value() != 0;
-    }
+    explicit operator bool() const;
             
     friend std::ostream& operator<<(std::ostream& s, const BigNumber& n);
     friend bool operator<(const BigNumber& a, const BigNumber& b);
@@ -79,121 +45,20 @@ public:
     friend bool operator>=(const BigNumber& a, const BigNumber& b);
     friend bool operator==(const BigNumber& a, const BigNumber& b);
     friend bool operator!=(const BigNumber& a, const BigNumber& b);
+    
+    friend BigNumber operator+(const BigNumber& a, const BigNumber& b);
+    friend BigNumber operator*(const BigNumber& a, const BigNumber& b);
             
 private:
-    static const std::vector<char> add(const std::vector<char>& a, const std::vector<char>& b) {
-        std::vector<char> result;
-        
-        char overflow = 0;
-        
-        auto end = std::max(a.size(), b.size());
-        for(int i = 0; i < end; ++i) {
-            overflow += i < a.size() ? a[i] : 0;
-            overflow += i < b.size() ? b[i] : 0;
-            
-            result.push_back(overflow % 10);
-            overflow /= 10;
-        }
-        
-        if(overflow) {
-            result.push_back(overflow);
-        }
-        
-        return result;
-    }
-    
-    static const std::vector<char> multiply(const std::vector<char>& a, const std::vector<char>& b) {
-        std::vector<char> result;
-        
-        for(int i = 0; i < a.size(); ++i) {
-            std::vector<char> temp(i, 0);
-            char overflow = 0;
-            
-            for(int j = 0; j < b.size(); ++j) {
-                overflow += a[i] * b[j];
-                temp.push_back(overflow % 10);
-                overflow /= 10;
-            }
-            
-            if(overflow) {
-                temp.push_back(overflow);
-            }
-            
-            result = add(result, temp);
-        }
-        
-        return result;
-    }
+    static const std::vector<char> add(const std::vector<char>& a, const std::vector<char>& b);
+    static const std::vector<char> multiply(const std::vector<char>& a, const std::vector<char>& b);
     
 private:
+    static BigNumber sMax;
+    
     mutable std::pair<bool, unsigned long> mCached;
     std::vector<char> mDigits;
+    bool mSign;
 };
-            
-inline std::ostream& operator<<(std::ostream& s, const BigNumber& n) {
-    for(int i = (int)n.mDigits.size() -1; i >= 0; --i) {
-        s << (int)n.mDigits[i];
-    }
-    
-    return s;
-};
-        
-inline bool operator<(const BigNumber& a, const BigNumber& b) {
-    if(a.digits() != b.digits()) {
-        return a.digits() < b.digits();
-    }
-    
-    for(int i = (int)a.digits()-1; i >= 0; --i) {
-        if(a.mDigits[i] != b.mDigits[i]) {
-            return a.mDigits[i] < b.mDigits[i];
-        }
-    }
-    
-    return false; //equal
-}
-
-inline bool operator>(const BigNumber& a, const BigNumber& b) {
-    return b < a;
-}
-
-inline bool operator<=(const BigNumber& a, const BigNumber& b) {
-    return !(a > b);
-}
-
-inline bool operator>=(const BigNumber& a, const BigNumber& b) {
-    return !(a < b);
-}
-
-inline bool operator==(const BigNumber& a, const BigNumber& b) {
-    if(a.digits() != b.digits()) {
-        return false;
-    }
-    
-    for(size_t i = 0; i < a.digits(); ++i) {
-        if(a.mDigits[i] != b.mDigits[i]) {
-            return false;
-        }
-    }
-    
-    return true;
-}
-
-inline bool operator!=(const BigNumber& a, const BigNumber& b) {
-    return !(a == b);
-}
-        
-inline BigNumber operator+(const BigNumber& a, const BigNumber& b) {
-    BigNumber result{a};
-    result += b;
-    
-    return result;
-}
-        
-inline BigNumber operator*(const BigNumber& a, const BigNumber& b) {
-    BigNumber result{a};
-    result *= b;
-    
-    return result;
-}
 
 #endif /* defined(__Euler__BigNumber__) */
